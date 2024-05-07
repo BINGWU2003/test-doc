e8e1be8ed6882c5fac0017ffab711b6b13c7d152
BINGWU <HuJiaCheng2003@163.com>
2024-05-07 21:49:45 +0800

    删除base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/test-doc/",
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
